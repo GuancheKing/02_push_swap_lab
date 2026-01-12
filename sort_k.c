0a9ff90c10be0548bbf243b1fea887b42dfa9ee6
josjimen <pepojimenezbethencourt@gmail.com>
2026-01-12 10:33:22 +0100

    wip sort_k.c

--- sort_k.c
+++ sort_k.c
@@ -6,7 +6,7 @@
 /*   By: josjimen <josjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 21:21:10 by josjimen          #+#    #+#             */
-/*   Updated: 2026/01/10 23:04:29 by josjimen         ###   ########.fr       */
+/*   Updated: 2026/01/12 10:00:26 by josjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	sort_k(t_node **a, t_node **b)
 {
-
+	int	n;
+	int	k;
+	
+	n = stack_size(*a);
+	k = choose_k(n);
+	
 }
 
 int	choose_k(int n)
@@ -26,4 +31,25 @@ int	choose_k(int n)
 		k = 20;
 	k = 45;
 	return (k);
+}
+
+void k_push_chunks(t_node **a, t_node **b, int n, int k)
+{
+	int low;
+	int high;
+	int mid;
+	int first_top;
+	int first_bot;
+
+	low = 0;
+	high = k -1;
+	mid = low + (high - low) / 2;
+	while (a != NULL)
+	{
+		if (exists_in_range(*a, low, high) != -1)
+		{
+			first_top = pos_first_in_range_from_top(*a, low, high);
+			first_bot = pos_first_in_range_from_bot(*a, low, high);
+		}
+	}
 }
