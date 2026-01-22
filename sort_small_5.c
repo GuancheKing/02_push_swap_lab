3d032ba4144716fac2131e19a6ee0ba11df0f2b1
josjimen <pepojimenezbethencourt@gmail.com>
2026-01-22 19:03:40 +0100

    sort_small_5 wip

--- sort_small_5.c
+++ sort_small_5.c
@@ -6,16 +6,39 @@
 /*   By: josjimen <josjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 19:29:06 by josjimen          #+#    #+#             */
-/*   Updated: 2026/01/22 18:22:12 by josjimen         ###   ########.fr       */
+/*   Updated: 2026/01/22 19:01:01 by josjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	pos_of_ind_from_top(t_node *a, int target)
+{
+	int 	pos;
+	t_node	*curr;
+	
+	pos = 0;
+	curr = a;
+	while (curr != NULL)
+	{
+		if (curr->ind == target)
+			return (pos);
+		curr = curr->nx;
+		pos++;
+	}
+	return (-1);
+}
+
 void	sort_small_5(t_node **a, t_node **b)
 {
-	int	n;
+	int		size;
+	int		to_push;
 
-	n = stack_size(a);
-	if (n)
+	size = stack_size(*a);
+	to_push = size - 3;
+	while (to_push > 0)
+	{
+		
+	}
+	
 }
